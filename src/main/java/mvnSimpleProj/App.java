e52e6efa2ecf8558270e6f5725752b5a89460c9c
Jomy <79013290+JomyDevOps@users.noreply.github.com>
2021-02-27 17:47:34 +0000

    Initial Commit

---
+++ src/main/java/mvnSimpleProj/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mvnSimpleProj;
+
+/**
+ * @author Window10
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello LYIT!");
+
+	}
+
+}
