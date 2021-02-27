a83117a2035cdd11ea77213f02f61ee86ed14066
Jomy <79013290+JomyDevOps@users.noreply.github.com>
2021-02-27 14:06:22 +0000

    Project refresh

---
+++ src/main/java/mvnProj/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package mvnProj;
+
+/**
+ * @author Window10
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello JOMY!");
+
+	}
+
+}
